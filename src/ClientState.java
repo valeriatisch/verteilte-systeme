d108cb110acc73d8a40c8d1802376513656a6a0b
ala4987 <77006334+ala4987@users.noreply.github.com>
2021-05-07 21:34:21 +0200

    Update ClientState.java

--- src/ClientState.java
+++ src/ClientState.java
@@ -1,24 +1,16 @@
 public class ClientState {
 
     private int state;
-    private int prevState;
     private String message;
     private String sender;
     private String receiver;
 
     public ClientState() {
         this.state = SMTPServer.serviceReady;
-        this.prevState = SMTPServer.serviceReady;
-        this.sender = "";
-        this.receiver = "";
-        this.message = "";
     }
 
     public int getState() {return this.state;}
     public void setState(int state) {this.state = state;}
-
-    public int getPrevState() {return this.prevState;}
-    public void setPrevState(int state) {this.prevState = state;}
 
     public String getMessage() {return this.message;}
     public void setMessage(String message) {this.message = message;}
