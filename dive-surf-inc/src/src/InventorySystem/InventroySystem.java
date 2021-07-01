516a891e969c86c4abfa3c647c49bc9ab97ddce7
Samy Hamdad <you@example.com>
2021-07-01 15:01:49 +0200

    Skeleton Result , Inventory

---
+++ dive-surf-inc/src/src/InventorySystem/InventroySystem.java
@@ -0,0 +1,14 @@
+/*
+InventorySystem: the inventory system takes transformed 
+order messages (see below) and simply tests whether the requested
+items are available. Only one inventory exists, 
+which means that the InventorySystems checks both types of items.
+Therefore, the inventory system modifies the valid property of the
+incoming messages and optional modifies the validationResult property
+*/
+
+package InventorySystem;
+
+public class InventroySystem {
+
+}
---
+++ dive-surf-inc/src/src/ResultSystem/Resultsystem.java
@@ -0,0 +1,23 @@
+/*
+ResultSystem: the result system simply collects
+ processed order and prints a message to the console once a new order is received.
+ The result system must distinguish between valid and invalid orders
+*/
+/*
+The result of the order application integration is a common/transformed format for orders.
+ Each order consists of following properties (type String):
+o CustomerID
+o FirstName
+o LastName
+o OverallItems (Number of all items in order)
+o NumberOfDivingSuits
+o NumberOfSurfboards
+o OrderID
+o Valid
+o validationResult
+*/
+package ResultSystem;
+
+public class Resultsystem {
+
+}
